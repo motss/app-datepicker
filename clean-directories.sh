9485b5461c59d110db345910395babaa9557fd5c
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-04-29 10:56:42 +0800

    :arrow_up: Upgraded dep to the latest, minor cleanup
    Signed-off-by: Rong Sen Ng <wes.ngrongsen@gmail.com>

---
+++ clean-directories.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+clear; printf "Running %s script...\n" "$(basename "$0" .sh)"
+
+has_unused_directories=false
+directories="dist/ bower_components/ node_modules/ .nyc_output/ coverage/ npm-debug.log yarn-error.log out/ coverage.lcov"
+
+for directory in $directories; do
+  if [ -d "$directory" ] || [ -f "$directory" ]; then
+    printf "\nRemoving %s...\n" "$directory"
+    rm -rf "$directory"
+    has_unused_directories=true
+  fi
+done
+
+if [ "$has_unused_directories" = true ]; then
+  printf "\nCleaning done.\n"
+else
+  printf "\nNothing to clean here.\n\n"
+fi
