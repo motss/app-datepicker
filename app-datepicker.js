d9562653a1696f936da8cc65a7137118e511c042
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-05-02 00:38:34 +0800

    :construction: Use Date object with UTC offset instead of date string
    Signed-off-by: Rong Sen Ng <wes.ngrongsen@gmail.com>

--- app-datepicker.js
+++ app-datepicker.js
@@ -28,7 +28,7 @@ class AppDatepicker extends Polymer.Element {
    *
    * @memberOf AppDatepicker
    */
-  get properties() {
+  static get properties() {
     return {
       disableDates: {
         type: Array,
@@ -36,14 +36,17 @@ class AppDatepicker extends Polymer.Element {
 
       disableDays: {
         type: Array,
+        value: () => [0, 6],
       },
 
       firstDayOfWeek: {
         type: String,
+        value: () => 0,
       },
 
       format: {
         type: String,
+        value: () => 'yyyy-mm-dd',
       },
 
       inputDate: {
@@ -52,14 +55,25 @@ class AppDatepicker extends Polymer.Element {
 
       locale: {
         type: String,
+        value: () => {
+          const localeFromNativeIntl = window.Intl
+            && window.Intl.DateTimeFormat
+            && new window.Intl.DateTimeFormat().resolvedOptions
+            && new window.Intl.DateTimeFormat().resolvedOptions().locale;
+          const localeFromNavigator = window.navigator.language;
+
+          return localeFromNativeIntl || localeFromNavigator || 'en-US';
+        },
       },
 
       maxDate: {
         type: Date,
+        value: () => new Date('9999/12/31').toJSON(),
       },
 
       minDate: {
         type: Date,
+        value: () => new Date('1970/1/1').toJSON(),
       },
 
       noAnimation: {
@@ -68,6 +82,8 @@ class AppDatepicker extends Polymer.Element {
 
       selectedDate: {
         type: Date,
+        readOnly: true,
+        reflectToAttribute: true,
       },
 
       selectedView: {
@@ -86,6 +102,20 @@ class AppDatepicker extends Polymer.Element {
    */
   get observedAttributes() {
     return [
+    ];
+  }
+
+  /**
+   * Observer changes to properties.
+   *
+   * @readonly
+   * @static
+   *
+   * @memberOf AppDatepicker
+   */
+  static get observers() {
+    return [
+
     ];
   }
 
@@ -110,6 +140,23 @@ class AppDatepicker extends Polymer.Element {
     super.connectedCallback();
 
     console.info('connected.');
+
+    const now = this.parseInputDate(this.inputDate) || new Date();
+
+    this._activeDateObject = {
+      fullYear: now.getFullYear(),
+      month: now.getMonth() + 1,
+      date: now.getDate(),
+      day: now.getDay(),
+      shortMonth: this.parseDateToShortMonth(now),
+      shortWeekday: this.parseDayToWeekday(now),
+    };
+
+    /* Update selectedDate based on inputDate or fallback Date object */
+    this._setSelectedDate(now.toJSON());
+
+    console.log(this._activeDateObject);
+    console.log(this.inputDate, this.selectedDate);
   }
 
   /**
@@ -123,6 +170,132 @@ class AppDatepicker extends Polymer.Element {
 
     console.info('disconnected.');
   }
+
+  /**
+   * Check if native Intl is supported.
+   *
+   * @returns {boolean} True if native Intl is supported.
+   *
+   * @memberOf AppDatepicker
+   */
+  hasNativeIntl() {
+    return window.Intl && window.Intl.DateTimeFormat;
+  }
+
+  /**
+   * Parse date into short month in text format.
+   *
+   * @param {Date} date - Date object.
+   * @returns {string} Short month in text format.
+   *
+   * @memberOf AppDatepicker
+   */
+  parseDateToShortMonth(date) {
+    if (this.hasNativeIntl) {
+      // const cachedLocale = this.properties.locale.value();
+      const cachedLocale = this.locale;
+
+      return new window.Intl.DateTimeFormat(cachedLocale, {
+        month: 'short',
+      }).format(date);
+    } else {
+      const getMonthFromDate = date.getMonth();
+
+      return AppDatepicker
+        .defaultMonths[getMonthFromDate]
+        .slice(0, 3);
+    }
+  }
+
+  /**
+   * Parse date into weekday in text format.
+   *
+   * @param {Date} date - Date object.
+   * @returns {string} weekday in text format.
+   *
+   * @memberOf AppDatepicker
+   */
+  parseDayToWeekday(date) {
+    if (this.hasNativeIntl) {
+      // const cachedLocale = this.properties.locale.value();
+      const cachedLocale = this.locale;
+
+      return new window.Intl.DateTimeFormat(cachedLocale, {
+        weekday: 'short',
+      }).format(date);
+    } else {
+      const getDayFromDate = date.getDay();
+
+      return AppDatepicker
+        .defaultWeekdays[getDayFromDate]
+        .slice(0, 3);
+    }
+  }
+
+  /**
+   * Parse input date into Date object.
+   *
+   * @param {Date} inputDate - Date object as input.
+   * @returns {Date} Valid Date object.
+   *
+   * @memberOf AppDatepicker
+   */
+  parseInputDate(inputDate) {
+    if (typeof inputDate === 'undefined'
+      || inputDate.toString() === 'Invalid Date') {
+      return null;
+    }
+
+    return inputDate;
+  }
+
+  /**
+   * Default weekdays in en-US.
+   *
+   * @readonly
+   * @static
+   *
+   * @memberOf AppDatepicker
+   */
+  static get defaultWeekdays() {
+    return [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+  }
+
+  /**
+   * Default months in en-US.
+   *
+   * @readonly
+   * @static
+   *
+   * @memberOf AppDatepicker
+   */
+  static get defaultMonths() {
+    return [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  }
+
 }
 
 window.customElements.define(AppDatepicker.is, AppDatepicker);
+
+// TODO: toUTCString or toJSON will return date string with UTC offset.
