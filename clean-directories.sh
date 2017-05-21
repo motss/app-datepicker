4c82f8a928756249eb632de4e182ce0c39badbb2
Rong Sen Ng <wes.ngrongsen@gmail.com>
2017-05-21 13:49:05 +0800

    :wrench: Minor update
    Signed-off-by: Rong Sen Ng <wes.ngrongsen@gmail.com>

--- clean-directories.sh
+++ clean-directories.sh
@@ -3,7 +3,7 @@
 clear; printf "Running %s script...\n" "$(basename "$0" .sh)"
 
 has_unused_directories=false
-directories="dist/ bower_components/ bower_components-1.x/ node_modules/ npm-debug.log yarn-error.log"
+directories="dist/ bower_components/ bower_components-1.x/ bower-1.x.json node_modules/ npm-debug.log yarn-error.log"
 
 for directory in $directories; do
   if [ -d "$directory" ] || [ -f "$directory" ]; then
