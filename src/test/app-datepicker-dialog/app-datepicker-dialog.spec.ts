4d2ec93ec99ec21a4b6900567d0be6b72246d9bc
Rong Sen Ng <wes.ngrongsen@gmail.com>
2019-12-13 21:00:30 +0800

    test: Removing WCT and installing WebDriverIO

--- src/test/app-datepicker-dialog/app-datepicker-dialog.spec.ts
+++
@@ -1,7 +0,0 @@
-import './a11y.spec.js';
-import './attributes.spec.js';
-import './custom-events.spec.js';
-// import './gestures.spec.js';
-import './initial-render.spec.js';
-import './keyboards.spec.js';
-import './properties.spec.js';
