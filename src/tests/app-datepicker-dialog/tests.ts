f1b29f0676dfadc6badee9d2c762b1940d885873
Rong Sen Ng <wes.ngrongsen@gmail.com>
2020-02-15 23:29:06 +0800

    test: Enable all tests

--- src/tests/app-datepicker-dialog/tests.ts
+++ src/tests/app-datepicker-dialog/tests.ts
@@ -1,8 +1,8 @@
 import './a11y.spec.js';
-// import './attributes.spec.js';
-// import './events.spec.js';
-// import './gestures.spec.js';
-// import './initial-render.spec.js';
-// import './keyboards.spec.js';
-// import './mouses.spec.js';
-// import './properties.spec.js';
+import './attributes.spec.js';
+import './events.spec.js';
+import './gestures.spec.js';
+import './initial-render.spec.js';
+import './keyboards.spec.js';
+import './mouses.spec.js';
+import './properties.spec.js';
