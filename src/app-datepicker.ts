694ff8d1b057cfec7ed5c3c0b5f83b17c49eabbc
Hannes Marth <hannes.marth@tui.at>
2020-09-21 16:11:37 +0200

    export added

--- src/app-datepicker.ts
+++ src/app-datepicker.ts
@@ -1,11 +1,12 @@
-import { DATEPICKER_NAME } from './constants.js';
-import { Datepicker } from './datepicker.js';
-import { customElementsDefine } from './helpers/custom-elements-define.js';
+import { DATEPICKER_NAME } from "./constants.js";
+import { Datepicker } from "./datepicker.js";
+import { customElementsDefine } from "./helpers/custom-elements-define.js";
 
 customElementsDefine(DATEPICKER_NAME, Datepicker);
 
 declare global {
   interface HTMLElementTagNameMap {
-    'app-datepicker': Datepicker;
+    "app-datepicker": Datepicker;
   }
 }
+export { customElementsDefine, Datepicker };
