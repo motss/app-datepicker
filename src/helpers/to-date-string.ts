f16c64d01e0c9ac891bded2beccff3be73cc567c
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2021-09-18 20:23:57 +0800

    test: add tests for toDateString

--- src/helpers/to-date-string.ts
+++ src/helpers/to-date-string.ts
@@ -1,8 +1,3 @@
 export function toDateString(date: Date): string {
-  try {
-    const dateString = date.toJSON();
-    return dateString == null ? '' : dateString.replace(/^(.+)T.+/i, '$1');
-  } catch {
-    return '';
-  }
+  return date.toISOString().replace(/^(.+)T.+/i, '$1');
 }
