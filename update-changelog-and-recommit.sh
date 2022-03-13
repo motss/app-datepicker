45629c45cbf66fe6cfd7ab76be30f0a456ac01e2
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-13 16:39:05 +0800

    build: minor update
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- update-changelog-and-recommit.sh
+++ update-changelog-and-recommit.sh
@@ -3,7 +3,7 @@
 # Grab last commit git message
 # LAST_COMMIT_MESSAGE=$(git log -1 --format=%s)
 
-printf "%b" "$(git log --online)"
+printf "%b" "$(git log --oneline)"
 
 # Update changelog and commit the changes
 sh generate-changelogs.sh
