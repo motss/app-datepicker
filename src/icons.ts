c2ee0208f5de2ee4b1c907375a2bce536602e8f2
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-26 14:08:27 +0800

    chore: remove unused comments
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- src/icons.ts
+++ src/icons.ts
@@ -1,8 +1,6 @@
 import { svg } from 'lit/static-html.js';
 
-// tslint:disable:max-line-length
 export const iconArrowDropdown = svg`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 10l5 5 5-5z"/></svg>`;
 export const iconChevronLeft = svg`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z"></path></svg>`;
 export const iconChevronRight = svg`<svg height="24" viewBox="0 0 24 24" width="24"><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path></svg>`;
 export const iconClear = svg`<svg height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>`;
-// tslint:enable:max-line-length
