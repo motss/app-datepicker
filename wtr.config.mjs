a3a754634e9c3863da3275173dc4e56056a698f5
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-11-19 17:25:39 +0800

    test: fix tests not running
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -1,10 +1,6 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
-import { fromRollup } from '@web/dev-server-rollup';
 import { sendKeysPlugin } from '@web/test-runner-commands/plugins';
 import { playwrightLauncher } from '@web/test-runner-playwright';
-
-const nodeResolvePlugin = fromRollup(nodeResolve);
 
 const {
   CI = 'false',
@@ -58,25 +54,8 @@ const config = {
   files: [
     `src/*tests*/${isTestHelpersOnly ? 'helpers' : '!(helpers)'}/*.test.ts`
   ],
+  nodeResolve: true,
   plugins: [
-    nodeResolvePlugin({
-      /**
-       * NOTE(motss): If you see the following warnings when running the tests, those are expected
-       * behavior when you run with the `development` version of `lit`.
-       *
-       * Using `dedupe: [id => /lit/i.test(id)]` or equivalent will not make the warnings go away.
-       *
-       * @example
-       *
-       * ```sh
-       * Lit is in dev mode. Not recommended for production! See https://lit.dev/msg/dev-mode for more information.
-       * Multiple versions of Lit loaded. Loading multiple versions is not recommended. See https://lit.dev/msg/multiple-versions for more information.
-       * ```
-       *
-       */
-      exportConditions: ['default', 'dev', 'development', 'esbuild', 'import'],
-      extensions: ['.mjs', '.js', '.ts', '.css', '.graphql'],
-    }),
     esbuildPlugin({ ts: true, target: 'es2021' }),
     sendKeysPlugin(),
   ],
